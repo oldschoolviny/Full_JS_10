fed5bafa25aff63d2b4709418211054c5b52b632
Valerii Bulakh <Valerabulakh@gmail.com>
2019-10-20 21:31:36 -0500

    lesson02

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 5755, 
+income = 'freelance',
+addExpenses = 'Food, House, Car, Gas', 
+deposit = true,
+mission = 2000,
+period = 4,
+budgetDay = 200;
 
-alert("hello Dude");
+//Методы и свойства
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log("Период " + period + ' месяцa'); 
+console.log("Цель заработать " + mission + ' долларов');
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
 
-console.log('JS Good');
+//Рузультат и остаток от деления
+console.log(money / 30, money % 2);
