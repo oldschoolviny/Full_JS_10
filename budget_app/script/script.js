ca88cb2632b5a678a6efc3fd485a27ce4b068cb6
Valerii Bulakh <oldschoolviny@gmail.com>
2019-11-04 22:32:55 -0600

    task1+2

--- budget_app/script/script.js
+++ budget_app/script/script.js
@@ -1,2 +1,37 @@
 'use strict';
 
+let start = document.getElementById('start');
+console.log(start);
+
+let buttonOne = document.getElementsByTagName('button')[0],
+    buttonTwo = document.getElementsByTagName('button')[1];
+console.log(buttonOne, buttonTwo);
+
+let checkBox = document.querySelector('#deposit-check');
+console.log(checkBox);
+
+let income = document.querySelectorAll('.additional_income-item');
+console.log(income);
+
+let blocksRight = document.querySelectorAll('[class$=-value]'); //не знаю как называеться
+console.log(blocksRight);                                /// Максим в видео [class$= - не пояснял как его делать то тем кто ен знает и нету знакомых??
+
+let mothIncome = document.querySelector('.salary-amount');
+console.log(mothIncome);
+
+let addIncomeNameInput = document.querySelector('input.income-title'),
+    addIncomeSumInput = document.querySelector('.income-amount');
+console.log(addIncomeNameInput, addIncomeSumInput);
+
+let manExpensesNameInput = document.querySelector('input.expenses-title'),
+    manExpensesSumInput = document.querySelector('.expenses-amount');
+console.log(manExpensesNameInput, manExpensesSumInput);
+
+let potencialExpenses = document.querySelector('.additional_expenses-item');
+console.log(potencialExpenses);
+
+let goal = document.querySelector('.target-amount');
+console.log(goal);
+
+let rangePeriod = document.querySelector('.period-select');
+console.log(rangePeriod);
