c2fc3fa0c26b8347a7c69713f01e5c56ac9af909
Valerii Bulakh <valerabulakh@gmail.com>
2019-11-19 19:11:26 -0600

    task 12

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     function countTimer(deadline){
         let timerHours = document.querySelector('#timer-hours'),
-            timerMinuts = document.querySelector('#timer-minutes'),
+            timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining(){
@@ -16,21 +16,25 @@ window.addEventListener('DOMContentLoaded', function(){
                 return {hours, minutes, seconds};
         }
 
-        function updateClock(){
+        function updateClock() {
             let timer = getTimeRemaining();
-        
-                timerHours.textContent = timer.hours;
-                timerMinuts.textContent = timer.minutes;
-                timerSeconds.textContent = timer.seconds;
-
-              
-                setTimeout(updateClock, 1000);
-            
+      
+            if (timer.hours < 10 ? timerHours.textContent =`0${timer.hours}`: timerHours.textContent = timer.hours);
+            if (timer.minutes < 10 ? timerMinutes.textContent =`0${timer.minutes}`: timerMinutes.textContent = timer.minutes);
+            if (timer.seconds < 10 ? timerSeconds.textContent =`0${timer.seconds}`: timerSeconds.textContent = timer.seconds);
+      
+            if (timer.timeRemaining > 0) {
+              setTimeout(updateClock, 1000);
+            } else if (timer.timerRemaining < 0) {
+              timerHours.textContent = '00';
+              timerMinutes.textContent = '00';
+              timerSeconds.textContent = '00';
+            }
+          }
+      
+          setInterval(updateClock, 1000);
         }
-
-        updateClock();
-    }
-
-    countTimer('31 december 2018');
+      
+        countTimer('22 november 2019');
 
 });
