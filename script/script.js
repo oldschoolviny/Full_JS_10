59dd36ed1b419d30737778b2b7fd6e2f28b35a03
Valerii Bulakh <valerabulakh@gmail.com>
2019-11-23 17:32:02 -0600

    task15

--- script/script.js
+++ script/script.js
@@ -123,4 +123,147 @@ window.addEventListener('DOMContentLoaded', function(){
 };
 
   tabs();
+
+// слайдер
+const slider = () => {
+    const slide  = document.querySelectorAll('.portfolio-item'),
+          btn    = document.querySelectorAll('.portfolio-btn'),
+          dotContainer = document.querySelector('.portfolio-dots'),
+          slider = document.querySelector('.portfolio-content');
+    let   dot;
+
+    let currentSlide = 0,
+        interval;
+
+    const createDots = () => {
+      slide.forEach(() => {
+        let newDot = document.createElement('li');
+        newDot.classList.add('dot');
+        dotContainer.appendChild(newDot);
+        dot = document.querySelectorAll('.dot');
+    });
+};
+
+createDots();
+
+const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+};
+
+const nextSlide = (elem, index, strClass) => {
+    elem[index].classList.add(strClass);
+};
+
+const autoPlaySlide = () => {
+      prevSlide(slide, currentSlide, 'portfolio-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+      currentSlide++;
+      if (currentSlide >= slide.length) {
+        currentSlide = 0;
+      }
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
+};
+
+const startSlide = (time = 3000) => {
+    interval = setInterval(autoPlaySlide, time);
+};
+
+const stopSlide = () => {
+    clearInterval(interval);
+};
+    
+    slider.addEventListener('click', (event) => {
+        event.preventDefault();
+        
+        let target = event.target;
+
+    if (!target.matches('.portfolio-btn, .dot')) {
+        return;
+    }
+
+    prevSlide(slide, currentSlide, 'portfolio-item-active');
+    prevSlide(dot, currentSlide, 'dot-active');
+      
+    if (target.matches('#arrow-right')) {
+        currentSlide++;
+    } else if (target.matches('#arrow-left')) {
+        currentSlide--;
+    } else if (target.matches('.dot')) {
+        dot.forEach((elem, index) => {
+            if (elem === target) {
+                currentSlide = index;
+            }
+        });
+    }
+
+    if (currentSlide >= slide.length) {
+        currentSlide = 0;
+    }
+    if (currentSlide < 0) {
+        currentSlide = slide.length - 1;
+    }
+
+    nextSlide(slide, currentSlide, 'portfolio-item-active');
+    nextSlide(dot, currentSlide, 'dot-active');
+});
+
+slider.addEventListener('mouseover', (event) => {
+    if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+        stopSlide();
+    }
+});
+
+slider.addEventListener('mouseout', (event) => {
+    if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+        startSlide();
+    }
+});
+
+    startSlide();
+};
+  
+    slider();
+
+// фотографии
+  const changePhoto = () => {
+      const photoBlock = document.getElementById('command');
+      
+      let photoFunction = (target) => {
+      let defaultSrc = target.src;
+          target.src = target.dataset.img;
+          target.dataset.img = defaultSrc;
+    };
+
+    photoBlock.addEventListener('mouseover', (event) => {
+        let target = event.target;
+        if (target.classList.contains('command__photo')) {
+        photoFunction(target);
+    }
+});
+
+    photoBlock.addEventListener('mouseout', (event) => {
+        let target = event.target;
+        if (target.classList.contains('command__photo')) {
+        photoFunction(target);
+    }
+});
+};
+
+  changePhoto();
+
+// регулярки для калькулятора
+const regInput = () => {
+    const inputBlock = document.querySelector('.calc-block');
+    
+    inputBlock.addEventListener('input', (event) => {
+        let target = event.target;
+        
+    if (target.classList.contains('calc-square') || target.classList.contains('calc-count') || target.classList.contains('calc-day')) {
+        target.value = target.value.replace(/\D/g, '');
+    }
+});
+};
+
+  regInput();
 });
